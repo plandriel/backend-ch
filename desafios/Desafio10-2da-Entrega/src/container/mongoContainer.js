b237fa514b3fa9ceebf295afa996f206d0f3abc8
Pedro Landriel <pedrolandriel@live.com.ar>
2022-11-07 21:16:58 -0300

    Re-entrega de 2da Entrega proyecto Final

--- desafios/Desafio10-2da-Entrega/src/container/mongoContainer.js
+++ desafios/Desafio10-2da-Entrega/src/container/mongoContainer.js
@@ -18,3 +18,88 @@ y dentro de dao/productMongoDAO vas a tener:
         }
     }
 */
+
+const { MongoClient, ObjectId } = require("mongodb");
+
+class MongoContainer {
+  constructor(connectionString, db, collect) {
+    this.mongo = new MongoClient(connectionString);
+    this.db = db;
+    this.collect = collect;
+  }
+  async connect() {
+    try {
+      await this.mongo.connect();
+      console.log("Connected to database");
+    } catch (error) {
+      throw error;
+    }
+  }
+
+  async getAll() {
+    try {
+      const data = await this.mongo
+        .db(this.db)
+        .collection(this.collection)
+        .find({})
+        .toArray();
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async getProductById(id) {
+    try {
+      const data = await this.mongo
+        .db(this.db)
+        .collection(this.collection)
+        .findOne({
+          _id: new ObjectId(id),
+        });
+      return data;
+    } catch (error) {
+      throw error;
+    }
+  }
+  async createProduct(data) {
+    try {
+      return await this.mongo
+        .db(this.db)
+        .collection(this.collection)
+        .insertOne(data);
+    } catch (error) {
+      throw error;
+    }
+  }
+  async updateProductById(id, data) {
+    try {
+      return await this.mongo
+        .db(this.db)
+        .collection(this.collection)
+        .updateOne(
+          {
+            _id: new ObjectId(id),
+          },
+          {
+            $set: data,
+          }
+        );
+    } catch (error) {
+      throw error;
+    }
+  }
+  async deleteProductById(id) {
+    try {
+      return await this.mongo
+        .db(this.db)
+        .collection(this.collection)
+        .deleteOne({
+          _id: new ObjectId(id),
+        });
+    } catch (error) {
+      throw error;
+    }
+  }
+}
+
+module.exports = MongoContainer;
