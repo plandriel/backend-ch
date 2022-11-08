f77290a45785b895fa2ff899c87784244f31ff50
Pedro Landriel <pedrolandriel@live.com.ar>
2022-11-07 21:11:57 -0300

    Correcccion del tutor

--- desafios/Desafio10-2da-Entrega/src/config/db.js
+++ desafios/Desafio10-2da-Entrega/src/config/db.js
@@ -3,10 +3,34 @@ import mongoose from "mongoose";
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGO_URI, (err) => {
-    err
-        ? console.log("⛔ Error al conectarse a MongoDB")
-        : console.log("🆗 Conectados a MongoDB")
-})
+// mongoose.connect(process.env.MONGO_URI, (err) => {
+//     err
+//         ? console.log("⛔ Error al conectarse a MongoDB")
+//         : console.log("🆗 Conectados a MongoDB")
+// })
 
-export default mongoose;
+// export default mongoose;
+
+const connectionDB={
+    connectionMongo: () => {
+        try{
+        console.log("Conectando a mongo...")
+            mongoose.connect(process.env.MONGO_URI, (err) => {
+                err
+                    ? console.log("⛔ Error al conectarse a MongoDB")
+                    : console.log("🆗 Conectados a MongoDB")
+            })
+        } catch (error){
+        console.log(error.message);
+        }
+    },
+    connectionFireBase: () => {
+        try {
+            console.log("Conectando a firebase...")
+        } catch (error) {
+            console.log("Ocurrio un error en la conexcion: ",error.message)
+        }
+    }
+}
+
+export default connectionDB;
